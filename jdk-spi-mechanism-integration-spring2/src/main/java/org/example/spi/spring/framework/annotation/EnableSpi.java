6e0ac2bc58259bf114527896d6ac43820047314b
pfnie <pfnie@yahoo.com>
2021-09-21 18:06:43 +0800

    d

--- jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/annotation/EnableSpi.java
+++ jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/annotation/EnableSpi.java
@@ -1,5 +1,6 @@
 package org.example.spi.spring.framework.annotation;
 
+import org.example.spi.spring.framework.registrar.ClasspathSpiRegistrar;
 import org.example.spi.spring.framework.registrar.ReflectionSpiRegistrar;
 import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
@@ -7,7 +8,7 @@ import java.lang.annotation.*;
 @Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 @Documented
-@Import(ReflectionSpiRegistrar.class)
+@Import(ClasspathSpiRegistrar.class)
 public @interface EnableSpi {
 
     String[] basePackages() default {};
