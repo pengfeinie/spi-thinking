334956c6777cc57f5019806c79b8bf77d6fb9a94
pfnie <pfnie@yahoo.com>
2021-09-23 12:45:02 +0800

    do

--- jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/registrar/ClassPathSpiScanner.java
+++ jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/registrar/ClassPathSpiScanner.java
@@ -1,69 +1,32 @@
 package org.example.spi.spring.framework.registrar;
 
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
 import java.util.Set;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
+import org.apache.commons.lang3.ClassUtils;
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinitionHolder;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
-import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.core.type.filter.AssignableTypeFilter;
-import org.springframework.core.type.filter.TypeFilter;
 
-/**
- * A {@link ClassPathBeanDefinitionScanner} that registers Mappers by
- * {@code basePackage}, {@code annotationClass}, or {@code markerInterface}. If
- * an {@code annotationClass} and/or {@code markerInterface} is specified, only
- * the specified types will be searched (searching for all interfaces will be
- * disabled).
- * <p>
- * This functionality was previously a private class of
- * {@link MapperScannerConfigurer}, but was broken out in version 1.2.0.
- *
- * @see MapperFactoryBean
- * @since 1.2.0
- * @version $Id$
- */
-public class ClassPathSpiScanner extends ClassPathBeanDefinitionScanner implements BeanFactoryAware {
-
-  private boolean addToConfig = true;
+public class ClassPathSpiScanner extends ClassPathScanningCandidateComponentProvider{
 
  
   private Class<? extends Annotation> annotationClass;
 
-  private Class<?> markerInterface;
-  
   private DefaultListableBeanFactory beanFactory;
 
-  public ClassPathSpiScanner(BeanDefinitionRegistry registry) {
-    super(registry, false);
+  public ClassPathSpiScanner(boolean useDefaultFilters, Class<? extends Annotation> annotationClass,DefaultListableBeanFactory beanFactory) {
+	super(useDefaultFilters);
+	this.annotationClass = annotationClass;
+	this.beanFactory = beanFactory;
   }
 
-  public void setAddToConfig(boolean addToConfig) {
-    this.addToConfig = addToConfig;
-  }
-
-  public void setAnnotationClass(Class<? extends Annotation> annotationClass) {
-    this.annotationClass = annotationClass;
-  }
-
-  public void setMarkerInterface(Class<?> markerInterface) {
-    this.markerInterface = markerInterface;
-  }
 
   /**
    * Configures parent scanner to search for the right interfaces. It can search
@@ -71,86 +34,29 @@ public class ClassPathSpiScanner extends ClassPathBeanDefinitionScanner implemen
    * those annotated with the annotationClass
    */
   public void registerFilters() {
-    boolean acceptAllInterfaces = true;
-
-    // if specified, use the given annotation and / or marker interface
     if (this.annotationClass != null) {
       addIncludeFilter(new AnnotationTypeFilter(this.annotationClass));
-      acceptAllInterfaces = false;
     }
-
-    // override AssignableTypeFilter to ignore matches on the actual marker interface
-    if (this.markerInterface != null) {
-      addIncludeFilter(new AssignableTypeFilter(this.markerInterface) {
-        @Override
-        protected boolean matchClassName(String className) {
-          return false;
-        }
-      });
-      acceptAllInterfaces = false;
-    }
-
-    if (acceptAllInterfaces) {
-      // default include filter that accepts all classes
-      addIncludeFilter(new TypeFilter() {
-        public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
-          return true;
-        }
-      });
-    }
-
-    // exclude package-info.java
-    addExcludeFilter(new TypeFilter() {
-      public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
-        String className = metadataReader.getClassMetadata().getClassName();
-        return className.endsWith("package-info");
-      }
-    });
-  }
-
-  /**
-   * Calls the parent search that will search and register all the candidates.
-   * Then the registered objects are post processed to set them as
-   * MapperFactoryBeans
-   */
-  @Override
-  public Set<BeanDefinitionHolder> doScan(String... basePackages) {
-    Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
-    if (beanDefinitions.isEmpty()) {
-      logger.warn("No MyBatis mapper was found in '" + Arrays.toString(basePackages) + "' package. Please check your configuration.");
-    } else {
-      for (BeanDefinitionHolder holder : beanDefinitions) {
-        GenericBeanDefinition definition = (GenericBeanDefinition) holder.getBeanDefinition();
-        Class<?> spiInterface = definition.getBeanClass();
-        ServiceLoader<?> services = ServiceLoader.load(spiInterface);
-        Iterator<?> iterator = services.iterator();
-        while(iterator.hasNext()){
-            Object service = iterator.next();
-            beanFactory.registerSingleton(spiInterface.getName(),service);
-        }
-      }
-    }
-    return Collections.emptySet(); 
   }
 
   @Override
+  public Set<BeanDefinition> findCandidateComponents(String basePackage) {
+	  	Set<BeanDefinition> beanDefinitions = super.findCandidateComponents(basePackage);
+	    for (BeanDefinition definition : beanDefinitions) {
+	    String spiInterfaceClassName = definition.getBeanClassName();
+	    List<Class<?>> spiInterfaceList = ClassUtils.convertClassNamesToClasses(Arrays.asList(spiInterfaceClassName));
+	    ServiceLoader<?> services = ServiceLoader.load(spiInterfaceList.get(0));
+	    Iterator<?> iterator = services.iterator();
+	    while(iterator.hasNext()){
+	        Object service = iterator.next();
+	        beanFactory.registerSingleton(service.getClass().getName(),service);
+	    }
+	  }
+	  return beanDefinitions;
+   }
+
+@Override
   protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
     return (beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().isIndependent());
   }
-
-  @Override
-  protected boolean checkCandidate(String beanName, BeanDefinition beanDefinition) throws IllegalStateException {
-    if (super.checkCandidate(beanName, beanDefinition)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-@Override
-public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-	this.beanFactory = (DefaultListableBeanFactory) beanFactory;
-	
-}
-
 }
--- jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/registrar/ClasspathSpiRegistrar.java
+++ jdk-spi-mechanism-integration-spring2/src/main/java/org/example/spi/spring/framework/registrar/ClasspathSpiRegistrar.java
@@ -3,7 +3,6 @@ package org.example.spi.spring.framework.registrar;
 
 import org.example.spi.spring.framework.annotation.EnableSpi;
 import org.example.spi.spring.framework.annotation.Spi;
-import org.reflections.Reflections;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -13,8 +12,6 @@ import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-
 import java.util.*;
 
 public class ClasspathSpiRegistrar implements ImportBeanDefinitionRegistrar,BeanFactoryAware {
@@ -25,9 +22,11 @@ public class ClasspathSpiRegistrar implements ImportBeanDefinitionRegistrar,Bean
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
     	List<String> basePackages = getBasePackages(importingClassMetadata);
-    	ClassPathSpiScanner scanner = new ClassPathSpiScanner(registry);
+    	ClassPathSpiScanner scanner = new ClassPathSpiScanner(true, Spi.class,beanFactory);
     	scanner.registerFilters();
-        scanner.doScan(StringUtils.toStringArray(basePackages));
+    	for (String packageName : basePackages) {
+    		 scanner.findCandidateComponents(packageName);
+    	}
     }
 
 
