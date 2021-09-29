f267111cbcb57dc7fd2f971ca6fba7ae816e803c
pfnie <pfnie@yahoo.com>
2021-09-29 13:54:56 +0800

    done

--- dubbo-spi-mechanism-thinking1/src/main/java/org/example/App.java
+++ dubbo-spi-mechanism-thinking1/src/main/java/org/example/App.java
@@ -4,13 +4,11 @@ import org.apache.dubbo.common.extension.ExtensionLoader;
 
 public class App {
 	
-    public static void main( String[] args ){   	
+    public static void main( String[] args ){
     	ExtensionLoader<SuperLogger> extensionLoader = ExtensionLoader.getExtensionLoader(SuperLogger.class);
     	SuperLogger xmlLogger = extensionLoader.getExtension("xmlLogger");
     	xmlLogger.configure();
     	SuperLogger jsonLogger = extensionLoader.getExtension("jsonLogger");
     	jsonLogger.configure();
-    	extensionLoader = ExtensionLoader.getExtensionLoader(SuperLogger.class);
-        System.out.println( "Hello World!" );
     }
 }
--- jdk-spi-mechanism-thinking2/src/main/java/org/example/XMLLogger.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class XMLLogger implements SuperLogger{
-
-	public void configure() {
-		System.out.println("xml config load success");
-	}
-
-}
