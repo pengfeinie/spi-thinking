75882298f260832890c29e16e09d9ed3fefed3c3
pfnie <pfnie@yahoo.com>
2021-09-30 11:16:18 +0800

    dubbo

--- dubbo-spi-mechanism-thinking1/src/main/java/org/example/App.java
+++ dubbo-spi-mechanism-thinking1/src/main/java/org/example/App.java
@@ -1,20 +1,17 @@
 package org.example;
 
-import org.apache.dubbo.common.extension.ExtensionFactory;
+
 import org.apache.dubbo.common.extension.ExtensionLoader;
-import org.apache.dubbo.common.extension.factory.AdaptiveExtensionFactory;
-import org.apache.dubbo.common.extension.factory.SpiExtensionFactory;
 
 public class App {
 	
     public static void main( String[] args ){
     	ExtensionLoader<SuperLogger> extensionLoader = ExtensionLoader.getExtensionLoader(SuperLogger.class);
+    	SuperLogger defaultLogger = extensionLoader.getDefaultExtension();
+    	defaultLogger.configure();
     	SuperLogger xmlLogger = extensionLoader.getExtension("xmlLogger");
     	xmlLogger.configure();
     	SuperLogger jsonLogger = extensionLoader.getExtension("jsonLogger");
     	jsonLogger.configure();
-		ExtensionFactory extensionFactory = ExtensionLoader.getExtensionLoader(ExtensionFactory.class).getAdaptiveExtension();
-		SuperLogger xmlLogger1 = extensionFactory.getExtension(SuperLogger.class, "xmlLogger");
-		System.out.println(xmlLogger1);
 	}
 }
--- dubbo-spi-mechanism-thinking1/src/main/java/org/example/SuperLogger.java
+++ dubbo-spi-mechanism-thinking1/src/main/java/org/example/SuperLogger.java
@@ -3,7 +3,7 @@ package org.example;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.common.extension.SPI;
 
-@SPI
+@SPI(value="xmlLogger")
 public interface SuperLogger {
 
 	@Activate
